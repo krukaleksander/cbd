8d2d3d2cb26ee0bd8dd30c10c01f530adab37334
Alek Kruk <kruk.aleksander@hotmail.com>
2021-02-28 18:10:39 +0100

    create header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Offer from './components/Offer';
 import Contact from './components/Contact';
 import AboutCbd from './components/AboutCbd';
 import Home from './components/Home';
 import Page404 from './components/Page404';
+import './css/style.min.css';
+import { FaBars } from 'react-icons/fa';
 
 const App = () => {
   return (
     <>
+      <header>
+        <h1 className='page-title'>cbdradzymin.pl</h1>
+        <FaBars className='menu-icon' />
+      </header>
       <Router>
         <div>
           <nav>
@@ -17,13 +23,13 @@ const App = () => {
               </li>
               <li>
                 <Link to='/oferta'>Oferta</Link>
-                </li>
-                <li>
-                  <Link to='/cbd'>O Cbd</Link>
-                </li>
-                <li>
-                  <Link to='kontakt'>Kontakt</Link>
-                </li>
+              </li>
+              <li>
+                <Link to='/cbd'>O Cbd</Link>
+              </li>
+              <li>
+                <Link to='kontakt'>Kontakt</Link>
+              </li>
             </ul>
           </nav>
           <Switch>
@@ -35,13 +41,13 @@ const App = () => {
             </Route>
             <Route path='/oferta'>
               <Offer />
-            </Route>            
+            </Route>
             <Route path='/'>
               <Home />
-            </Route> 
+            </Route>
             <Route path="*">
-            <Page404/>
-            </Route>                      
+              <Page404 />
+            </Route>
           </Switch>
         </div>
       </Router>
